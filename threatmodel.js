31485caeea6ee16c829b8bb7fb4fdbf33f822d19
Joe Andrieu <joe@legreq.com>
2026-03-30 13:34:23 -0700

    working through JSONify

---
+++ threatmodel.js
@@ -0,0 +1,8 @@
+var ThreatModel = {
+  render: function render() { console.log("render", arguments); },
+  register: function register() { console.log("register", arguments); },
+  validate: function validate() { console.log("validate", arguments); }
+} 
+
+export default ThreatModel;
+
